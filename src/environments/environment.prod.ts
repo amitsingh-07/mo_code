2e340b3c13c91459485e0f8ec28a75105ce8d238
Haider Ali N <haiderali.n@hcl.com>
2023-05-23 12:01:28 +0530

    configJsonUrl - updated in prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -26,7 +26,7 @@ export const environment: IEnvironment = {
   expire2faPollRate: 2,
   expire2faMaxCheck: 5,
   promoCodeJsonUrl: 'https://mo-static-assets.s3-ap-southeast-1.amazonaws.com/promo/prod/promo-details.json',
-  configJsonUrl: 'https://mo-static-assets.s3.ap-southeast-1.amazonaws.com/promo/KS098130_config.json',
+  configJsonUrl: 'https://mo-static-assets.s3.ap-southeast-1.amazonaws.com/config/app-config.json',
   hsPortalId: '6324163',
   hsUrlTrack: '3e764f66-0b93-4f91-8e29-75fdf4dc9177',
   singpassClientId: 't0asVg7hrn6OsAWoFiM9oBaFQfwShtrn',
